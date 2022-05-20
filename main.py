955e5d4f4351a1217365ba88f17c5646bda04b5b
ShaunR23 <shaunradford23@gmail.com>
2022-05-20 13:44:23 -0400

    added levels, detect game over

--- main.py
+++ main.py
@@ -8,7 +8,27 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car.create_car()
+    car.move_cars()
+
+    for x in car.all_cars:
+        if x.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+    if player.ycor() > 280:
+        player.reset_position()
+        car.level_up()
+        scoreboard.increase_score()
+
+screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,27 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("green")
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.goto(-280, 250)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_score(self):
+        self.level += 1
+        self.update_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.color("red")
+        self.write("GAME OVER", align="center", font=FONT)
